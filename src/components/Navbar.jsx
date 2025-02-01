cf3cd6ff211d827b7147e9f1e38b986b07e65471
frkkos-61 <faruk.kos61@gmail.com>
2025-02-02 01:37:17 +0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,118 @@
+import React, { useEffect, useRef, useState } from "react";
+import { TiLocationArrow } from "react-icons/ti";
+import Button from "./Button";
+import { useWindowScroll } from "react-use";
+import gsap from "gsap";
+
+const navItems = ["Nexus", "Vault", "Prologue", "About", "Contact"];
+
+const Navbar = () => {
+  const [isAudioPlaying, setisAudioPlaying] = useState(false);
+  const [isIndıcatorActive, setisIndıcatorActive] = useState(false);
+  const navContainerRef = useRef(null);
+  const audioElementRef = useRef(null);
+
+  const { y: currentScrollY } = useWindowScroll();
+  const [lastScrollY, setlastScrollY] = useState(0);
+  const [isNavVisible, setIsNavVisible] = useState(true);
+
+  const toggleAudiAudiIndicator = () => {
+    setisAudioPlaying((prev) => !prev);
+
+    setisIndıcatorActive((prev) => !prev);
+  };
+
+  useEffect(() => {
+    if (isAudioPlaying) {
+      audioElementRef.current.play();
+    } else {
+      audioElementRef.current.pause();
+    }
+  }, [isAudioPlaying]);
+
+  useEffect(() => {
+    if (currentScrollY === 0) {
+      setIsNavVisible(true);
+      navContainerRef.current.classList.remove("floating-nav");
+    } else if (currentScrollY > lastScrollY) {
+      setIsNavVisible(false);
+      navContainerRef.current.classList.add("floating-nav");
+    } else if (currentScrollY < lastScrollY) {
+      setIsNavVisible(true);
+      navContainerRef.current.classList.add("floating-nav");
+    }
+    setlastScrollY(currentScrollY);
+  }, [currentScrollY, lastScrollY]);
+
+  useEffect(() => {
+    gsap.to(navContainerRef.current, {
+      y: isNavVisible ? 0 : -100,
+      opacity: isNavVisible ? 1 : 0,
+      duration: 0.5,
+    });
+  }, [isNavVisible]);
+
+  useEffect(() => {
+    if (isAudioPlaying) {
+      audioElementRef.current.play();
+    } else {
+      audioElementRef.current.pause();
+    }
+  }, [isAudioPlaying]);
+
+  return (
+    <div
+      ref={navContainerRef}
+      className="fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duratin-700 sm:inset-x-6"
+    >
+      <header className="absolute top-1/2 w-full -translate-y-1/2">
+        <nav className="flex size-full items-center justify-between p-4">
+          <div className="flex items-center gap-7">
+            <img src="/img/logo.png" alt="logo" className="w-10" />
+
+            <Button
+              id="product-button"
+              title="Products"
+              rightIcon={<TiLocationArrow />}
+              containerClass="bg-white md:flex hidden items-center justify-center gap-1"
+            />
+          </div>
+          <div className="flex h-full items-center">
+            <div className="hidden md:block">
+              {navItems.map((item) => (
+                <a
+                  key={item}
+                  href={`#${item.toLowerCase()}`}
+                  className="nav-hover-btn"
+                >
+                  {item}
+                </a>
+              ))}
+            </div>
+            <button
+              className="ml-10 flex items-center space-x-0.5"
+              onClick={toggleAudiAudiIndicator}
+            >
+              <audio
+                ref={audioElementRef}
+                className="hidden"
+                src="/audio/loop.mp3"
+                loop
+              />
+              {[1, 2, 3, 4].map((bar) => (
+                <div
+                  key={bar}
+                  className={`indicator-line ${
+                    isIndıcatorActive ? "active" : ""
+                  }`}
+                />
+              ))}
+            </button>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Navbar;
